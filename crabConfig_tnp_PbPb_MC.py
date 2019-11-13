09131b5cd8eb54b06063e6039ff083e6c7bab532
Marek Walczak <marekwalczak@gmail.com>
2019-11-13 15:52:01 +0100

    first commit...

---
+++ crabConfig_tnp_PbPb_MC.py
@@ -0,0 +1,32 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.section_('General')
+config.General.requestName = 'TnP_HINPbPbAutumn18DR-mva98_HadroJPsi_MC_201911113'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.section_('JobType')
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'tnp_PbPb_MC.py'
+config.JobType.maxMemoryMB = 4000
+config.JobType.maxJobRuntimeMin = 2750
+config.JobType.allowUndistributedCMSSW = True
+
+config.section_('Data')
+config.Data.inputDataset = '/JPsi_pThat-2_TuneCP5_HydjetDrumMB_5p02TeV_Pythia8/HINPbPbAutumn18DR-mva98_103X_upgrade2018_realistic_HI_v11-v1/AODSIM'
+config.Data.inputDBS = 'global'
+config.Data.unitsPerJob = 50
+config.Data.splitting = 'FileBased'
+config.Data.outLFNDirBase = '/store/user/%s/TagAndProbe/PbPb2018/TnP/%s' % (getUsernameFromSiteDB(), config.General.requestName)
+config.Data.publication = False
+config.Data.allowNonValidInputDataset = True
+config.Data.outputDatasetTag = config.General.requestName
+
+config.section_('Site')
+config.Data.ignoreLocality = True
+config.Site.whitelist = ['T1_FR_*','T1_US_*','T2_FR_*','T2_US_*','T2_CH_CERN']
+#config.Site.storageSite = 'T2_CH_CERN'
+config.Site.storageSite = 'T2_US_Vanderbilt'
+#config.Site.storageSite = 'T2_PL_Swierk'
---
+++ crabConfig_tnp_PbPb_data.py
@@ -0,0 +1,34 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.section_('General')
+config.General.requestName = 'TnP_HIForward_ReReco_PbPb5TeV_2018_JPsi_Data_20191113'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.section_('JobType')
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'tnp_PbPb_data.py'
+config.JobType.maxMemoryMB = 4000
+config.JobType.maxJobRuntimeMin = 1220
+config.JobType.allowUndistributedCMSSW = True
+
+config.section_('Data')
+config.Data.inputDataset ='/HIForward/HIRun2018A-04Apr2019-v1/AOD'
+config.Data.allowNonValidInputDataset = True
+config.Data.inputDBS = 'global'
+config.Data.unitsPerJob = 50
+config.Data.splitting = 'LumiBased'
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/HI/PromptReco/Cert_326381-327564_HI_PromptReco_Collisions18_JSON_HF_and_MuonPhys.txt'
+config.Data.runRange = '326381-327564'
+config.Data.outLFNDirBase = '/store/user/%s/TagAndProbe/PbPb2018/TnP/%s' % (getUsernameFromSiteDB(), config.General.requestName)
+config.Data.publication = False
+config.Data.outputDatasetTag = config.General.requestName
+
+config.section_('Site')
+config.Data.ignoreLocality = True
+config.Site.whitelist = ['T1_US_*','T2_US_*','T1_FR_*','T2_FR_*','T2_CH_CERN']
+#config.Site.storageSite = 'T2_CH_CERN'
+config.Site.storageSite = 'T2_US_Vanderbilt'
+#config.Site.storageSite = 'T2_PL_Swierk'
