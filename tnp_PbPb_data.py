b3c2bf7f67a41dd1338d2a31e34cc026878524e1
Marek Walczak <marekwalczak@gmail.com>
2019-11-13 16:39:50 +0100

    adding changes...

--- tnp_PbPb_data.py
+++ tnp_PbPb_data.py
@@ -12,6 +12,7 @@ process.source = cms.Source("PoolSource",
 )
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
+
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
